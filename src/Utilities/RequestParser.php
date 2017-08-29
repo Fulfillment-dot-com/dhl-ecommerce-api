f64cb4f2f3c61a747376062dbec9ce15677a3789
FoxxMD <matt.duncan13@gmail.com>
2017-08-29 12:47:26 -0400

    Refactor error parsing
    Flatten errors into 1d array

--- src/Utilities/RequestParser.php
+++ src/Utilities/RequestParser.php
@@ -13,30 +13,67 @@ use GuzzleHttp\Exception\RequestException;
 
 class RequestParser
 {
-    /**
-     * Returns an object or array of the error parsed from the Guzzle Request exception
-     * @param RequestException $requestException
-     * @param bool             $isAssoc
-     * @return string
-     */
-    public static function parseError(RequestException $requestException, $isAssoc = true)
-    {
+	/**
+	 * Returns an object or array of the error parsed from the Guzzle Request exception
+	 *
+	 * @param RequestException $requestException
+	 * @param bool             $isAssoc
+	 *
+	 * @return mixed
+	 */
+	public static function parseError(RequestException $requestException, $isAssoc = true)
+	{
 
-        $error = $error = json_decode($requestException->getResponse()->getBody(), $isAssoc);
+		$response = json_decode($requestException->getResponse()->getBody(), $isAssoc);
 
-        if (null !== $error) {
-            return $error->meta->error;
-        }
-            return $requestException->getMessage();
-    }
+		if (null !== $response)
+		{
+			return self::searchErrors($response);
+		}
 
-    public static function getErrorCode(RequestException $requestException)
-    {
-        $error = $error = json_decode($requestException->getResponse()->getBody());
+		return $requestException->getMessage();
+	}
 
-        if (null !== $error && isset($error->meta->error)) {
-            return $error->meta->error[0]->error_type;
-        }
-            return null;
-    }
+	public static function getErrorCode(RequestException $requestException)
+	{
+		$error = $error = json_decode($requestException->getResponse()->getBody());
+
+		if (null !== $error && isset($error->meta->error))
+		{
+			return $error->meta->error[0]->error_type;
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param $array
+	 * @param $path
+	 *
+	 * @return array
+	 */
+	protected static function searchErrors(array $array, array $path = [])
+	{
+		$errors = [];
+		foreach ($array as $key => $val)
+		{
+			if(is_array($val)) {
+				$nextLevelPath   = $path;
+				$nextLevelPath[] = $key;
+				if($key === 'errors') {
+					foreach ($val as $error)
+					{
+						$errors[] = array_merge($error, ['path' => implode('/', $nextLevelPath)]);
+					}
+				} else {
+					$nextLevelErrors = self::searchErrors($val, $nextLevelPath);
+					if (count($nextLevelErrors) > 0)
+					{
+						$errors = array_merge($errors, $nextLevelErrors);
+					}
+				}
+			}
+		}
+		return $errors;
+	}
 }
